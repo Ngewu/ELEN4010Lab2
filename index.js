d50b45f27bf05f8f1e97bde1185e2fce48ca3185
Enkosi Ngewu <engewu@gmail.com>
2022-05-09 20:13:55 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = require('path') // used later in the exercise
+const express = require('express')
+const app = express()
+
+const mainRouter = require('./mainRoutes')
+
+app.use(mainRouter) 
+
+const port = process.env.PORT || 3000 
+app.listen(port)
+console.log('Express server running on port', port)
+
+mainRouter.get('/', function (req, res) { res.send('Hello World. I\'m a Node app.')
+})
